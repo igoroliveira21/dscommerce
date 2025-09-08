01cb496deba13f7b83913d230fd7cf1c48229fa4
igoroliveira21 <igoron182017@gmail.com>
2025-09-08 13:14:14 -0300

    get categories

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.dto.CategoryDTO;
+import com.devsuperior.dscommerce.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+    
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> dto = categoryService.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+
+}
